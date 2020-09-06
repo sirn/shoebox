294133d532ee702d51a77477b0cdf6f74d6cf17e
Kridsada Thanabulpong <sirn@ogsite.net>
2020-09-06 20:22:02 +0900

    Initial version that display something.

--- src/shoebox.nim
+++ src/shoebox.nim
@@ -6,12 +6,43 @@ import shoebox/epd2in7b
 proc main() =
   var gpio = newGPIO()
   var spiDev = newSPIDev(0, 0)
+  var epd2in7b = newEpd2in7b(gpio, spiDev)
 
   defer:
-    gpio.close()
+    epd2in7b.powerOff()
     spiDev.close()
+    gpio.close()
 
-  discard newEpd2in7b(gpio, spiDev)
+  var blackFb: EpdFb = epd2in7b.convertFb(@[
+    0'u8, 0'u8, 0'u8, 0'u8, 0'u8, 0'u8, 0'u8, 0'u8, 0'u8, 0'u8,
+    0'u8, 1'u8, 1'u8, 0'u8, 1'u8, 1'u8, 0'u8, 1'u8, 1'u8, 0'u8,
+    0'u8, 1'u8, 1'u8, 0'u8, 1'u8, 1'u8, 0'u8, 1'u8, 1'u8, 0'u8,
+    0'u8, 1'u8, 1'u8, 0'u8, 1'u8, 1'u8, 0'u8, 1'u8, 1'u8, 0'u8,
+    0'u8, 1'u8, 1'u8, 1'u8, 1'u8, 1'u8, 0'u8, 1'u8, 1'u8, 0'u8,
+    0'u8, 1'u8, 1'u8, 1'u8, 1'u8, 1'u8, 0'u8, 1'u8, 1'u8, 0'u8,
+    0'u8, 1'u8, 1'u8, 0'u8, 1'u8, 1'u8, 0'u8, 1'u8, 1'u8, 0'u8,
+    0'u8, 1'u8, 1'u8, 0'u8, 1'u8, 1'u8, 0'u8, 1'u8, 1'u8, 0'u8,
+    0'u8, 1'u8, 1'u8, 0'u8, 1'u8, 1'u8, 0'u8, 1'u8, 1'u8, 0'u8,
+    0'u8, 0'u8, 0'u8, 0'u8, 0'u8, 0'u8, 0'u8, 0'u8, 0'u8, 0'u8,
+  ], 10, 10, EpdColor.ecBlack)
+
+  var redFb: EpdFb = epd2in7b.convertFb(@[
+    0'u8, 0'u8, 0'u8, 0'u8, 0'u8, 0'u8, 0'u8, 0'u8, 0'u8, 0'u8, 0'u8, 0'u8,
+    0'u8, 0'u8, 0'u8, 0'u8, 0'u8, 0'u8, 0'u8, 0'u8, 0'u8, 0'u8, 1'u8, 1'u8,
+    0'u8, 0'u8, 0'u8, 0'u8, 0'u8, 0'u8, 0'u8, 0'u8, 0'u8, 0'u8, 1'u8, 1'u8,
+    0'u8, 0'u8, 0'u8, 0'u8, 0'u8, 0'u8, 0'u8, 0'u8, 0'u8, 0'u8, 1'u8, 1'u8,
+    0'u8, 0'u8, 0'u8, 0'u8, 0'u8, 0'u8, 0'u8, 0'u8, 0'u8, 0'u8, 1'u8, 1'u8,
+    0'u8, 0'u8, 0'u8, 0'u8, 0'u8, 0'u8, 0'u8, 0'u8, 0'u8, 0'u8, 1'u8, 1'u8,
+    0'u8, 0'u8, 0'u8, 0'u8, 0'u8, 0'u8, 0'u8, 0'u8, 0'u8, 0'u8, 0'u8, 0'u8,
+    0'u8, 0'u8, 0'u8, 0'u8, 0'u8, 0'u8, 0'u8, 0'u8, 0'u8, 0'u8, 1'u8, 1'u8,
+    0'u8, 0'u8, 0'u8, 0'u8, 0'u8, 0'u8, 0'u8, 0'u8, 0'u8, 0'u8, 1'u8, 1'u8,
+    0'u8, 0'u8, 0'u8, 0'u8, 0'u8, 0'u8, 0'u8, 0'u8, 0'u8, 0'u8, 0'u8, 0'u8,
+  ], 12, 10, EpdColor.ecRed)
+
+  epd2in7b.renderFb(blackFb, EpdColor.ecBlack)
+  epd2in7b.renderFb(redFb, EpdColor.ecRed)
+  epd2in7b.displayRefresh()
+  epd2in7b.deepSleep()
 
 
 when isMainModule:
--- src/shoebox/spidev.nim
+++ src/shoebox/spidev.nim
@@ -20,29 +20,35 @@ type
 
 
 proc setMode*(a: SPIDev, mode: int) =
+  ## Set SPI mode of a SPIDev device.
   if posix.ioctl(a.fd, SPI_IOC_WR_MODE, unsafeAddr mode) < 0:
     raise newException(SPIDevError, fmt"failed to set mode to {mode}")
 
 
 proc setBitsPerWord*(a: SPIDev, bitsPerWord: int) =
+  ## Set bits per word of a SPIDev device.
   if posix.ioctl(a.fd, SPI_IOC_WR_BITS_PER_WORD, unsafeAddr bitsPerWord) < 0:
     raise newException(SPIDevError, fmt"failed to set bits_per_word to {bitsPerWord}")
 
 
 proc setMaxSpeedHz*(a: SPIDev, maxSpeedHz: int) =
+  ## Set max speed hz of a SPIDev device.
   if posix.ioctl(a.fd, SPI_IOC_WR_MAX_SPEED_HZ, unsafeAddr maxSpeedHz) < 0:
     raise newException(SPIDevError, fmt"failed to set max_speed_hz to {maxSpeedHz}")
 
 
 proc writeByte*(a: SPIDev, b: byte) =
+  ## Write a single byte to SPIDev.
   discard posix.write(a.fd, unsafeAddr b, 1)
 
 
 proc close*(a: SPIDev) =
+  ## Close a SPIDev.
   discard posix.close(a.fd)
 
 
 proc newSPIDev*(bus: int, device: int): SPIDev =
+  ## Setup a SPIDev with the given bus and device ID.
   var spiFd: FileHandle
   var path = fmt"/dev/spidev{bus}.{device}"
 
